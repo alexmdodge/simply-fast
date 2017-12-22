e5c1789a023fa6fa5e31cb4a85f3b3d33db543ef
Alex Dodge <alex.dodge@redspace.com>
2017-12-22 09:17:59 -0400

    SF-1: remove old features - Remove old version of webpack and stub out build file to support updated loaders and flow

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,3 @@
-require('../styles/styles.css');
-import {SimpleDOM} from './support/simpleDOM';
-
-var dom = new SimpleDOM(true);
-
-dom.append('splash-description', 'The best website ever!');
-dom.append('splash-description', 'But there are errors.');
-dom.append('splash-description', 'But there are errors.');
-
+/**
+ * Root application script
+ */
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,34 @@
-var webpack = require('webpack');
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: './src/scripts/app.js',
-  output: {
-    path: './src/scripts',
-    filename: 'app.bundle.js'
+  entry: {
+    app: "./src/scripts/app.js",
+    vendor: "./src/scripts/vendor.js",
   },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    } , { 
-      test: /\.css$/,
-      loader: "style!css" 
-    } , {
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      loaders: [
-        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-      ]
-    }]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-      output: {
-        comments: false,
-      },
-    }),
 
-    new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development, 
-      // ./public directory is being served 
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: 'src', index: 'index.html' },
-      files: './src/*.html'
-    })
-  ]
- };
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    publicPath: "/assets/",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+      },
+    ],
+  },
+
+  resolve: {
+
+    modules: [
+      "node_modules",
+      path.resolve(__dirname, "src")
+    ],
+
+    extensions: [".js", ".json", ".css"],
+  },
+
+  devtool: "source-map",
+}
